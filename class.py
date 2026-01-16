3fb10bbe0722bf34a2d5bea5955fe43595926668
pheebemi <lemuelemmanuel29@gmail.com>
2026-01-16 11:17:23 +0100

    today we did collections

---
+++ class.py
@@ -0,0 +1,26 @@
+#this code below is for a print statement
+
+#print('hello world')
+
+name = 'Emmanuel'
+isStudent = True
+age = 50
+gpa = 5.0
+
+# print(name)
+# print(isStudent)
+# print(age)
+# print(gpa)
+
+# print(type(isStudent))
+
+
+# print(f'my name is {name} my age is {age} and my gpa is {gpa}')
+
+name =bool(name)
+isStudent =str(isStudent)
+age =bool(age)
+gpa=int(gpa)
+
+
+print(age)
---
+++ input.py
@@ -0,0 +1,355 @@
+    # name = input('Enter your name')
+
+    # age = input("enter your age")
+    # age =int(age)
+
+    # age += 1
+
+
+    # print(f"hello {name} Happy Birthday youre {age} years old")
+
+    # length = float(input("Enter the length "))
+    # breath = float(input("Enter the breath"))
+
+    # area = length*breath
+
+    # print(f" th area of the rectangle is {area}")
+
+    #shopping cart
+
+    # item = input("Enter the item name")
+    # price = float(input("enter the price"))
+    # quantity = int(input("enter the quantity"))
+
+    # total = price*quantity
+
+    # print(f"your item{item} total price is ${total}")
+
+    # balance = 0.00
+    # deposit  = float(input("Add ammount"))
+
+    # current_balance = balance+deposit
+    # print(f'Your current balance is ${current_balance}')
+
+    # withdrawal = float(input("Enter withdarwal Ammount"))
+
+    # present_balance = current_balance - withdrawal
+    # if present_balance < 0:
+    #     print("insufficeint Balance")
+
+    # else:
+    #     print(f"your present balance after withdrawal {present_balance}")
+
+
+    # income = 100000
+
+    # rent = float(input("enter your rent"))
+    # electricity = float(input("enter your electricity"))
+    # phone = float(input("enter your phone"))
+    # family = float(input("enter your family"))
+    # children = float(input("enter your children"))
+    # water = float(input("enter your water"))
+    # education = float(input("enter your education"))
+
+    # total_expenses = rent + electricity + phone + family + children + water + education
+    # total_savings = income-total_expenses
+
+    # print(f"your total expenses for the month is ${total_expenses}")
+    # if total_expenses > income:
+    #     print("we are in debt")
+    # else:
+    #     print(f"you saved ${total_savings} for the month ")
+
+
+
+    # radius = float(input(' enter the radius'))
+
+    # circumference = (2* math.pi * radius)
+
+    # print(f'the circumference of a circle is {circumference}')
+
+    # b = 50
+    # c = 11
+    # print(round(a))
+    # print(math.floor(a))
+    # print(math.ceil(a))
+    # print(math.e)
+    # print(pow(a,6))
+    # print(max(a,b,c))
+    # print(min(a,b,c))
+    # a = 5
+
+
+    # #a = a % 2
+
+    # print(math.sqrt(a))
+    # import math
+    # import cmath
+
+    # a = float(input('enter A'))
+    # b = float(input('enter b'))
+    # c = float(input('enter c'))
+
+    # x = (-b - math.sqrt(pow(b,2)) - math.sqrt(4*a*c)) / 2*a
+
+    # print(x)
+
+    # age = int(input(' enter your age'))
+
+    # if age >=100:
+    #     print(' youre too old to vote')
+    # elif age>=18:
+    #     print('youre eligable to vote')
+    # elif age<=0:
+    #     print(' youre not yet born')
+    # else:
+    #     print('youre not eligible to vote')
+
+    # response = input('are you a student Y/N')
+
+    # if response == 'Y':
+    #     print(' Welcome to school')
+    # elif response == 'N':
+    #     print('youre not a student')
+    # else: 
+    #     print('wrong input')
+
+    # score = int(input('enter your score'))
+
+    # if score >100:
+    #     print(f'enter a score less than 100 {score}')
+    # elif score >=70:
+    #     print(f'grade is A {score}')
+    # elif score >=60:
+    #     print(f'grade is B {score}')
+    # elif score >=50:
+    #     print(f'grade is C {score}')
+    # elif score >=45:
+    #     print(f'grade is D {score}')
+    # elif score >=40:
+    #     print(f'grade is E {score}')
+    # elif score <40 and score >=0:
+    #     print(f'grade is F {score}')
+    # else:
+
+
+    #     print(f'Score Cant be less than Zero {score}')
+
+    # am = float(input('enter the actual value'))
+    # mm = float(input('enter the measured value'))
+
+    # pe = ((mm-am)/am) * 100
+
+    # error = mm-am
+
+    # print(f'The percentage error is {pe}')
+
+    # if error == 0.5:
+    #     print('the measurement is perfect')
+    # elif error >0.5:
+    #     print('its greater')
+    # else:
+    #     print('its lesser')
+
+    # import math
+
+    # num1 = float(input('enter the first number'))
+    # operator = input('enter the operator')
+    # num2 = float(input('enter the second number'))
+
+    # if operator == '*':
+    #     print(num1 * num2)
+    # elif operator == '/':
+    #     print(num1 / num2)
+    # elif operator == '-':
+    #     print(num1 - num2)
+    # elif operator == '+':
+    #     print(num1 + num2)
+    # elif operator == 'sin':
+    #     print(round( math.sin(num1), 4))
+    # elif operator == 'cos':
+    #     print(round(math.cos(num1),4))
+    # elif operator == 'tan':
+    #     print(round(math.tan(num1), 4))
+    # elif operator == 'log':
+    #     print (round(math.log(num1), 4))   
+    # else:
+    #     print(f'{operator} is not a valid operator')
+
+
+# temp = 25
+# is_raining = False
+
+# if temp <= 25  and is_raining:
+#         print('the weather is fine')
+
+# elif temp >=25 or not is_raining:
+#     print('its cloudy')
+# else:
+#         print('its raining')
+
+num = 40
+age = 19
+
+# print('positve' if num > 0 else 'negetive')
+# print('odd' if num % 2 else 'even')
+# print('adult' if age >=18 else 'chlid')
+
+
+#methods
+
+
+
+
+#result= username.rfind('o')
+#result = username.capitalize()
+#result =username.upper()
+#result = username.lower()
+#result = username.isalpha()
+#result = username.isdigit()
+# result = username.count('o')
+
+# print(result)
+
+
+
+# username = input('enter your name: ')
+
+
+# if len(username) <=8 or >=12 :
+#     print(' enter a username less than 12 characters')
+# elif  username.find(' ') :
+#     print('your username contains spaces')
+# elif not username.isalpha():
+#     print('your username should be all letters')
+# else:
+#     print(f'welcome {username}')
+
+# credit_number = '1234-3344-4242-4535'
+# last_digits = credit_number[-4:]
+
+# print(f'xxxx-xxxx-xxxx-{last_digits}')
+
+# price1 =3.14453
+# price2 = 5544343434.0095
+# price3 = 343.432
+
+
+# print(f'your price is ${price1:+,.2f}')
+# print(f'your price is ${price2:+,.2f}')
+# print(f'your price is ${price3:+,.2f}')
+
+# name = input('enter your name')
+
+
+# while name == '':
+#     print('you didnt enter a name ')
+#     name = input('enter your name')
+
+# print(f'welcome{name}')
+
+# age = int(input('enter your age '))
+
+# while age <0:
+#     print('enter a valid age')
+#     age = int(input('enter your age '))
+# print(f'your age is {age}')
+
+
+# food = input('enter a food you like or q to quit')
+
+# while not food == 'q':
+#     print(f'i like {food}')
+#     food = input('enter a food you like or q to quit')
+# print('bye')
+
+
+# num = int(input('enter a number between 1-10: '))
+
+# while num <1 or num >10:
+#     print('enter a valid number')
+#     num = int(input('enter a number between 1-10: '))
+
+# print(num)
+
+# principal = 0
+# rate = 0
+# time= 0
+
+# while principal <=0:
+#     principal = float(input('enter the principal'))
+#     if principal <=0:
+#         print('number should not be less than or equals to zero')
+# while rate <=0:
+#     rate = float(input('enter the rate'))
+#     if rate <=0:
+#         print('number should not be less than or equals to zero')
+# # while time <=0:
+# #     time = float(input('enter the time'))
+# #     if time <=0:
+# #         print('number should not be less than or equals to zero')
+
+# # simple_interest= (principal*rate*time)/100
+
+# # print(f'in {time}years your simple intrest is ${simple_interest}')
+
+# for x in range(1,20):
+#     if x == 13:
+#         break
+#     else:
+#         print(x)
+
+# import time
+
+# my_time = int(input('enter time in seconds'))
+
+# for x in range(my_time,0, -1):
+
+#     seconds= x % 60
+#     minutes = int(x/60) % 60
+#     hrs = int(x/3600)
+#     print(f'{hrs}:{minutes}:{seconds}')
+    
+#     time.sleep(1)
+
+# print('times up')
+
+# rows = int(input('enter the number of rows: '))
+# columns = int(input('enter the number of colums'))
+# symbol = int(input('enter the symbol'))
+
+# for y in range(rows):
+#     for x in range(columns):
+#         print(symbol, end='')
+#     print()
+
+# n = int(input('enter the height'))
+
+# for i in range(n):
+#     for j in range(n-i-1):
+#         print(' ' ,end=' ')
+#     for k in range(i+1):
+#         print('# #', end=' ')
+#     print()
+
+
+fruits = {'apple', 'pineapple', 'mango', 'banana', 'orange'}
+
+#print(fruits[0])
+
+#print(len(fruits))
+# for fruit in fruits:
+
+# print('apple' in fruits)
+
+# fruits.insert(0, 'mango')
+# fruits.append('orange')
+# fruits.remove('apple')
+# fruits.clear()
+# fruits.sort()
+# fruits.reverse()
+# fruits.add('orange')
+fruits.pop()
+
+
+print(fruits)
